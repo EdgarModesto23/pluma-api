e3fcab96f2e649594bdc82ceb7178c7233a18912
Edgar Modesto <edgarmodesto23@gmail.com>
2024-02-23 02:43:45 -0700

    fixed mail errors

--- pluma_app/handlers/board.py
+++ pluma_app/handlers/board.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import receiver
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import viewsets
 from rest_framework.decorators import action
@@ -7,6 +8,14 @@ from pluma_users.models import User
 import pluma_app.serializers.board_serializers as serializers
 from pluma_app.models import Board
 from rest_framework import permissions
+from django.core.mail import EmailMultiAlternatives
+from django.core.exceptions import ValidationError
+from django.core.validators import EmailValidator
+from pluma_users.models import User
+from django.conf import settings
+from email.mime.image import MIMEImage
+from django.contrib.staticfiles import finders
+from django.template.loader import render_to_string
 
 
 class boardViewSet(viewsets.ModelViewSet):
@@ -52,9 +61,8 @@ class boardViewSet(viewsets.ModelViewSet):
         try:
             user = User.objects.get(email=data["email"])
         except ObjectDoesNotExist:
-            return Response(
-                {"error": "User does not exist"}, status=status.HTTP_404_NOT_FOUND
-            )
+            self.sendInvite(sender=self.request.user, recipients=data["email"])
+            return Response({"message": "Email sent"}, status=status.HTTP_200_OK)
 
         if data["action"] == "add":
             board.allowed_users.add(user)
@@ -73,3 +81,30 @@ class boardViewSet(viewsets.ModelViewSet):
                     {"error": "User is not in the board"},
                     status=status.HTTP_404_NOT_FOUND,
                 )
+
+    def logo_data(self, path, cid):
+        print("hey")
+        with open(finders.find(path), "rb") as f:
+            logo_data = f.read()
+        logo = MIMEImage(logo_data)
+        logo.add_header("Content-ID", f"<{cid}>")
+        return logo
+
+    def sendInvite(self, sender, recipients) -> bool:
+        print("chavalos")
+        subject = f"Join {sender} in taking notes on Pluma!"
+        message = f"{sender} invited you to join his board on pluma!"
+        from_email = settings.DEFAULT_FROM_EMAIL
+        recipients_list = [recipients]
+        html_message = render_to_string(
+            "../templates/invite.html", context={"sender": sender}
+        )
+        message = EmailMultiAlternatives(
+            subject=subject, body=message, from_email=from_email, to=recipients_list
+        )
+        message.attach_alternative(html_message, "text/html")
+        message.attach(self.logo_data(path="Pluma.png", cid="Pluma"))
+        message.attach(self.logo_data(path="github.png", cid="github"))
+        message.send(fail_silently=False)
+        print("hey")
+        return True
